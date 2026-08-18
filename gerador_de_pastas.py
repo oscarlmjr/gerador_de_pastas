9cd850c85bb02cfdb6fd09b3f11b3effdd6936ab
oscarlmjr <oscarlmjr@gmail.com>
2026-08-18 13:03:58 -0300

    Adicionando gerador de título underline

--- gerador_de_pastas.py
+++ gerador_de_pastas.py
@@ -1,15 +1,25 @@
-from pathlib import Path
 import os
+import unidecode
 
 
 with open('texto2.txt', 'r', encoding='utf8') as arquivo:
-	
-	for nova_pasta in arquivo:
-		nome_da_pasta = nova_pasta.strip()
-		nome_arquivo = ".gitkeep"
-		caminho_completo = os.path.join(nome_da_pasta, nome_arquivo)
-		
-		os.makedirs(nome_da_pasta, exist_ok=True)
 
-		with open(caminho_completo, 'w', encoding='utf8') as file:
-			print(nome_da_pasta)
+    for titulo in arquivo:
+        nova_string = ''
+
+        for caracter in titulo:
+            if caracter.isalpha() or caracter.isdigit():
+                nova_string += caracter
+            else:
+                nova_string += '_'
+
+        nome_da_pasta = unidecode.unidecode(nova_string)
+        print(nome_da_pasta)
+        nome_arquivo = ".gitkeep"
+
+        caminho_completo = os.path.join(nome_da_pasta, nome_arquivo)
+
+        os.makedirs(nome_da_pasta, exist_ok=True)
+
+        with open(caminho_completo, 'w', encoding='utf8') as file:
+            print(nome_da_pasta)
